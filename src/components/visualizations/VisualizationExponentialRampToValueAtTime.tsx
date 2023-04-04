fda355b00546993a0c3881d17fe9e50af9a5e4bd
George Satellite <lunaerxs@gmail.com>
2023-04-04 19:59:56 +0300

    desc + code: exponentialRampToValueAtTime

--- src/components/visualizations/VisualizationExponentialRampToValueAtTime.tsx
+++ src/components/visualizations/VisualizationExponentialRampToValueAtTime.tsx
@@ -1,4 +1,16 @@
 import { type AudioProcessingSchedulerFn, Visualization } from "./Visualization"
+import { Paragraph, Highlight } from "./Paragraph"
+
+const Description = () => {
+  return (
+    <Paragraph>
+      Schedules a gradual <Highlight>exponential</Highlight> change in the value of the AudioParam.
+    </Paragraph>
+  )
+}
+
+const code =
+`param.exponentialRampToValueAtTime(maxValue, startTime + duration)`
 
 export const VisualizationExponentialRampToValueAtTime = () => {
   const scheduleAudioProcessing: AudioProcessingSchedulerFn = ({
@@ -13,6 +25,8 @@ export const VisualizationExponentialRampToValueAtTime = () => {
   return (
     <Visualization
       title="exponentialRampToValueAtTime"
+      description={<Description/>}
+      code={code}
       scheduleAudioProcessing={scheduleAudioProcessing}
     />
   )
