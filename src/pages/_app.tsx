05105469fa6f91f98336e235122294f53a66cc11
George Satellite <lunaerxs@gmail.com>
2023-04-06 23:43:29 +0300

    twitter card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 const title = 'AudioParam Visualization'
 const description = 'Visualization of how Web Audio API\'s AudioParam value changes over time'
+const ogImage = '/og.png'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,7 +22,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content={description}/>
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/og.png" />
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@isatelllte"/>
+        <meta name="twitter:title" content={title}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={ogImage}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
