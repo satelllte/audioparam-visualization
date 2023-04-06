4f838436ff4bc20c672b2eb3b4c2378c3fec606d
George Satellite <lunaerxs@gmail.com>
2023-04-06 23:38:47 +0300

    meta - og:image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content={description}/>
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
+        <meta property="og:image" content="/og.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
