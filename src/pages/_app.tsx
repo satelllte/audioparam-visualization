5e300b9cc269496506218698cc0b521ed3afc81d
satelllte <lunaerxs@gmail.com>
2023-04-06 23:47:08 +0300

    Meta tags (#9)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import type { AppProps } from 'next/app'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const title = 'AudioParam Visualization'
+const description = 'Visualization of how Web Audio API\'s AudioParam value changes over time'
+const ogImage = '/og.png'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -14,10 +18,18 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <Head>
-        <title>AudioParam Visualization</title>
-        <meta name="description" content="AudioParam Visualization" />
+        <title>{title}</title>
+        <meta name="description" content={description}/>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@isatelllte"/>
+        <meta name="twitter:title" content={title}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={ogImage}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
     </>
