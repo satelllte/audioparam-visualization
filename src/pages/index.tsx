f738c60c034a018af94b5a930a08f5813356e7a1
George Satellite <lunaerxs@gmail.com>
2023-04-07 11:00:12 +0300

    footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { IconGitHub } from '@/components/icons/IconGitHub'
 import {
   VisualizationSetValueAtTime,
   VisualizationLinearRampToValueAtTime,
@@ -28,6 +30,25 @@ const HomePage = () => {
         <VisualizationCancelAndHoldAtTime/>
         <VisualizationCancelScheduledValues/>
       </main>
+      <footer className='mt-2 py-8 text-sm text-white-dim'>
+        <div className='px-4 max-w-3xl mx-auto'>
+          <div className='before:content-["-"] before:pr-1'>
+            {'Made by: '}<Link
+              href='https://github.com/satelllte'
+              target='_blank'
+              className='font-semibold border-b border-b-transparent hover:text-white hover:border-b-white motion-safe:transition-colors'
+            >@satelllte</Link>
+          </div>
+          <div className='before:content-["-"] before:pr-1'>
+            {'Source code: '}<Link
+              href='https://github.com/satelllte/audioparam-visualization'
+              target='_blank'
+              className='font-semibold inline-flex gap-1 items-center border-b border-b-transparent hover:text-white hover:border-b-white motion-safe:transition-colors'
+            >GitHub <IconGitHub/>
+            </Link>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
